b783093b8038167d2e2b6ada2de8d6bd53f7d740
Zach Zhao <thezachzhao@gmail.com>
2021-01-01 17:03:45 -0500

    Start repo with basic routes

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const { Router } = require("express");
+const express = require("express");
+const app = express();
+
+// Import Routes
+const authRoute = require('./routes/auth');
+
+// Route Middlewares
+app.use('/api/user', authRoute);
+
+
+app.listen(3000, () => {console.log('Up and running')});
+
